ea6b88b03bf442d2a1a9db8b87c1b7bfa8c23525
5and3Eduardo <5and3eduardo@gmail.com>
2021-02-21 20:55:37 +0000

    Refactoring seeders

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -4,20 +4,44 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Customer;
+
 
 class CustomerSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Seed database using a csv file.
      *
      * @return void
      */
     public function run()
     {
+        $tableName = app(Customer::class)->getTable();
+        // Csv file path
         $path = 'C:/xampp/htdocs/rockar-tech-test/storage/app/private/sql_files/customer.csv';
-        DB::table('customers')->delete();
-        $sql = file_get_contents('./database/seeds/customers.sql');
-        $sql = sprintf($sql, $path);
-        DB::connection()->getpdo()->exec($sql);
+        DB::table('customers')->truncate();
+
+        try{
+            //  Populate table using csv file
+            $sql = "LOAD DATA INFILE '%s' 
+                    IGNORE INTO TABLE %s
+                    CHARACTER SET utf8 FIELDS 
+                    TERMINATED BY ',' 
+                    ENCLOSED BY '\"' 
+                    ESCAPED BY '' 
+                    LINES TERMINATED BY '\n' 
+                    IGNORE 1 LINES 
+                    (email,forename,surname,contact_number,postcode, @created_at, @updated_at) 
+                SET created_at=NOW(), updated_at=NOW()";
+
+            $sql = sprintf($sql, $path, $tableName);
+            DB::connection()->getpdo()->exec($sql);
+
+        } catch(\PDOException $e) {
+
+            echo $e->getMessage();
+            
+        }
+        
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Product;
 
 class ProductSeeder extends Seeder
 {
@@ -14,10 +15,33 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
+        // Csv file path
+        $tableName = app(Product::class)->getTable();
         $path = 'C:/xampp/htdocs/rockar-tech-test/storage/app/private/sql_files/product.csv';
-        DB::table('products')->delete();
-        $sql = file_get_contents('./database/seeds/products.sql');
-        $sql = sprintf($sql, $path);
-        DB::connection()->getpdo()->exec($sql);
+        DB::table('products')->truncate();
+
+        try{
+            //  Populate table using csv file
+            $sql = "LOAD DATA INFILE '%s' 
+                    IGNORE INTO TABLE %s 
+                    CHARACTER SET utf8 FIELDS 
+                    TERMINATED BY ',' 
+                    ENCLOSED BY '\"' 
+                    ESCAPED BY '' 
+                    LINES TERMINATED BY '\n' 
+                    IGNORE 1 LINES 
+                    (vin,colour,make,model,price, @created_at, @updated_at) 
+                SET created_at=NOW(), updated_at=NOW()";
+
+            $sql = sprintf($sql, $path, $tableName);
+            DB::connection()->getpdo()->exec($sql);
+        } 
+        
+        catch(\PDOException $e) {
+
+            echo $e->getMessage();
+            
+        }
+        
     }
 }

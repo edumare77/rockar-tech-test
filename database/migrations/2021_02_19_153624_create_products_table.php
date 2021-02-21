d8ec5dea4d9bdf1f602e63e6270c7b78d2ff898f
5and3Eduardo <5and3eduardo@gmail.com>
2021-02-21 18:34:12 +0000

    Migrations

---
+++ database/migrations/2021_02_19_153624_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('vin', 17);
+            $table->string('colour');
+            $table->string('make');
+            $table->string('model');
+            $table->float('price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
